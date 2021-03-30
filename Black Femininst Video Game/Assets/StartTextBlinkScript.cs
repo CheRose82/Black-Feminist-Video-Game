fc3571325da53c79d686f5906ca7e6638a43d34c
Che Rose <cr7450a@student.american.edu>
2021-03-30 01:36:08 -0400

    Title Screen and bugs
    Created the Title screen. It's basic, someone is more than welcome to make it better. Also finally fixed the Dialogue System bug. This push should be ready for the interns tomorrow.

---
+++ Black Femininst Video Game/Assets/StartTextBlinkScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartTextBlinkScript : MonoBehaviour
+{
+    public Text thistext;
+    public bool TextEnabled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextEnabled = true;
+        Blink();
+
+    }
+
+
+    public void Blink()
+    {
+        if(TextEnabled == true)
+        {
+            thistext.enabled = false;
+            TextEnabled = false;
+        }
+        else
+        {
+            thistext.enabled = true;
+            TextEnabled = true;
+        }
+        Cycle();
+    }
+
+    public void Cycle()
+    {
+        Invoke("Blink", 1);
+    }
+}
