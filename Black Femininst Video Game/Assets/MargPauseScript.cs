fb6b5e69328a34429e44c35f8c7daf7bfb88877f
Che Rose <cr7450a@student.american.edu>
2021-04-05 16:10:28 -0400

    Received Dialogue database from Arjanae

---
+++ Black Femininst Video Game/Assets/MargPauseScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MargPauseScript : MonoBehaviour
+{
+    public Text thisText;
+    public Image thisImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(thisText.enabled == true)
+            {
+                thisText.enabled = false;
+                thisImage.enabled = false;
+            }
+            else
+            {
+                thisText.enabled = true;
+                thisImage.enabled = true;
+            }
+        }
+    }
+}
