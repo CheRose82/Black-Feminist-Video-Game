a49b9d51c79c4a26c614afe65b2372d541b403c7
Che Rose <cr7450a@student.american.edu>
2021-04-19 02:21:51 -0400

    Going to bed

---
+++ Black Femininst Video Game/Assets/karenExplodeScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karenExplodeScript : MonoBehaviour
+{
+    public GameObject jonas;
+    public GameObject sabine;
+    public bool recoiling;
+    public GameObject db55Nooo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jonas = GameObject.Find("Player");
+        sabine = GameObject.Find("Sabine");
+    }
+
+    void Update()
+    {
+        if (recoiling)
+        {
+            jonas.transform.position = Vector3.MoveTowards(jonas.transform.position, jonas.transform.position + new Vector3(-6, 0, 0), 3 * Time.deltaTime);
+            sabine.transform.position = Vector3.MoveTowards(sabine.transform.position, sabine.transform.position + new Vector3(6, 0, 0), 3 * Time.deltaTime);
+            
+        }  
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            jonas.GetComponent<Rigidbody>().AddForce(-50, 150, 0);
+            sabine.GetComponent<Rigidbody>().AddForce(50, 150, 0);
+            Invoke(nameof(StopRecoiling), 1.5f);
+           
+        }
+    }
+
+    public void StopRecoiling()
+    {
+        recoiling = false;
+    }
+}
--- Black Femininst Video Game/Assets/realmHolderScript.cs
+++ Black Femininst Video Game/Assets/realmHolderScript.cs
@@ -13,6 +13,7 @@ public class realmHolderScript : MonoBehaviour
     public GameObject jonas;
     public GameObject sabine;
 
+
     public bool moving;
     public bool movingLeft;
 
@@ -25,6 +26,34 @@ public class realmHolderScript : MonoBehaviour
     public bool movingApart;
     public float movingApartSpeed;
 
+    public GameObject cam;
+
+    public GameObject spotlights;
+
+    public GameObject movingBeam;
+    public bool beamActivated;
+    public bool beamMovingLeft;
+    public float beamZAngle;
+    public float beamRotateSpeed;
+
+    public GameObject theaterBackground;
+    public GameObject whiteBackground;
+    public GameObject db54;
+
+    public GameObject wallFlasher;
+
+    public GameObject KarenSpawner;
+    public GameObject karen;
+
+    public GameObject leftCurtain;
+    public GameObject rightCurtain;
+    public GameObject LCurtainDest;
+    public GameObject RCurtainDest;
+    public bool stageExpanding;
+    public GameObject topLights;
+
+    public GameObject bombKaren;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,12 +65,29 @@ public class realmHolderScript : MonoBehaviour
         jonasPos = GameObject.Find("JonasJumpToPos");
         jonas = GameObject.Find("Player");
         sabine = GameObject.Find("Sabine");
+        spotlights = GameObject.Find("Spotlights");
+        movingBeam = GameObject.Find("Moving Beam Holder");
+        theaterBackground = GameObject.Find("Theater Background");
+        whiteBackground = GameObject.Find("White Theater Background");
+        wallFlasher = GameObject.Find("Wall Flasher");
+        KarenSpawner = GameObject.Find("Karen Spawner");
+        leftCurtain = GameObject.Find("Left Curtain");
+        rightCurtain = GameObject.Find("Right Curtain");
+        LCurtainDest = GameObject.Find("LCurtainDest");
+        RCurtainDest = GameObject.Find("RCurtainDest");
+        
+
+        spotlights.SetActive(false);
+        movingBeam.SetActive(false);
+        whiteBackground.SetActive(false);
 
         Invoke(nameof(StartLevelDialogue), 2);
         Invoke(nameof(MoveToStage), 8);
         Invoke(nameof(Places), 8);
+        Invoke(nameof(LightsOn), 8.75f);
 
         movingIPTimer = 1.5f;
+        beamRotateSpeed = .05f;
     }
 
     private void Update()
@@ -74,8 +120,11 @@ public class realmHolderScript : MonoBehaviour
             {
                 movingIntoPlace = false;
                 movingIPTimer = 1.5f;
-                MoveApart();
-                p26Dialogue();
+                Invoke(nameof(MoveApart), 1);
+                Invoke(nameof(p26Dialogue), 2);
+                //MoveApart();
+                //p26Dialogue();
+                
             }
         }
 
@@ -86,6 +135,52 @@ public class realmHolderScript : MonoBehaviour
             sabine.transform.Translate(movingApartSpeed, 0, 0);
 
         }
+
+        //once Jonas and Sabine are manually touching again, manually make them hold hands.
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            jonas.transform.position = jonasPos.transform.position;
+            sabine.transform.position = new Vector3(jonas.transform.position.x + 1.3f, sabine.transform.position.y, sabine.transform.position.z);
+            movingApart = false;
+            cam.transform.position = new Vector3(196.68f, 2.8f, -3.22f);
+            Invoke(nameof(ReturnCam), 3f);
+        }
+
+        //make the beam move from left to right
+        if (beamActivated)
+        {
+            beamZAngle = movingBeam.transform.rotation.z;
+            if (beamMovingLeft)
+            {
+                //float zAngle;
+                movingBeam.transform.Rotate(0, 0, -beamRotateSpeed);
+                //beamZAngle -= .01f;
+                if(beamZAngle < -.3f)
+                {
+                    beamMovingLeft = false;
+                }
+            }
+            else
+            {
+                movingBeam.transform.Rotate(0, 0, beamRotateSpeed);
+                //beamZAngle += .01f;
+                if (beamZAngle > .3f)
+                {
+                    beamMovingLeft = true;
+                }
+            }
+        }
+
+        //make the curtains expand and the level grow
+        if (stageExpanding)
+        {
+            leftCurtain.transform.position = Vector3.MoveTowards(leftCurtain.transform.position, LCurtainDest.transform.position, .2f * Time.deltaTime);
+            rightCurtain.transform.position = Vector3.MoveTowards(rightCurtain.transform.position, RCurtainDest.transform.position, .2f * Time.deltaTime);
+            jonas.transform.Translate(-.01f, 0, 0);
+            sabine.transform.Translate(.01f, 0, 0);
+        }
+        
+
         
 
 
@@ -149,6 +244,69 @@ public class realmHolderScript : MonoBehaviour
     public void p26Dialogue()
     {
         Instantiate(WhatsHappDBp26, transform.position, Quaternion.identity);
+    }
+
+    public void LightsOn()
+    {
+        spotlights.SetActive(true);
+    }
+
+    public void ReturnCam()
+    {
+        cam.transform.position = new Vector3(196.31f, 5.08f, -11.02f);
+        //turn on the moving jbeam just after that
+        Invoke(nameof(TurnOnBeam), 1);
+        Invoke(nameof(WhiteBackground), 5);
+    }
+
+    public void TurnOnBeam()
+    {
+        movingBeam.SetActive(true);
+        beamActivated = true;
+    }
+
+    public void WhiteBackground()
+    {
+        whiteBackground.SetActive(true);
+        theaterBackground.SetActive(false);
+
+        //start the images of whiteness wall flashing
+        Invoke(nameof(ImageFlashes), 2.0f);
+        Invoke(nameof(SpawnKarens), 8f);
+        Invoke(nameof(ExpandStage), 13);
+        Invoke(nameof(DropBombKaren), 18);
+
+        //didn't need to do that
+        Instantiate(db54, transform.position + new Vector3(0, 100, 0), Quaternion.identity);
+    }
+
+    public void DBp54()
+    {
+        Instantiate(db54, transform.position + new Vector3(0, 100, 0), Quaternion.identity);
+    }
+
+    public void ImageFlashes()
+    {
+        wallFlasher.GetComponent<WallFlasherScript>().activated = true;
+    }
+
+    public void SpawnKarens()
+    {
+        //Instantiate(karen, KarenSpawner.transform.position, Quaternion.identity);
+        Instantiate(karen, KarenSpawner.transform.position + new Vector3(-4, 0, 0), Quaternion.identity);
+        Instantiate(karen, KarenSpawner.transform.position + new Vector3(4, 0, 0), Quaternion.identity);
+        Instantiate(karen, KarenSpawner.transform.position + new Vector3(-7, 0, 0), Quaternion.identity);
+        Instantiate(karen, KarenSpawner.transform.position + new Vector3(7, 0, 0), Quaternion.identity);
+    }
+
+    public void ExpandStage()
+    {
+        stageExpanding = true;
+    }
+
+    public void DropBombKaren()
+    {
+        Instantiate(bombKaren, KarenSpawner.transform.position + new Vector3(0, 20, 0), Quaternion.identity);
     }
 
 
