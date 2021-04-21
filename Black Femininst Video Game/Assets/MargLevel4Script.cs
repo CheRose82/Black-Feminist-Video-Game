5f9726ff49584b727297b982e2028e830b1a4d75
Che Rose <cr7450a@student.american.edu>
2021-04-21 15:28:20 -0400

    marg animations made not implimented

---
+++ Black Femininst Video Game/Assets/MargLevel4Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MargLevel4Script : MonoBehaviour
+{
+    public GameObject dbFinal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(dbFinal, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
